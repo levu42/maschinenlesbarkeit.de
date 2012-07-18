98bff6a575dfd58b43ebbd8a39f610cdbddee8f0
levu <levu@alderaan.scienceco.de>
2012-07-18 11:46:02 +0200

    fix for includes in APIs

--- api.php
+++ api.php
@@ -11,10 +11,13 @@
 	$GLOBALS['apis'] = array();
 
 	if (isset($config['enabled-apis']) && is_array($config['enabled-apis'])) {
+		$dir = getcwd();
 		foreach($config['enabled-apis'] as $api) {
 			$GLOBALS['apis'][$api] = json_decode(file_get_contents('./' . $api . '/api.json'), true);
-			$GLOBALS['apis'][$api]['init'] = require_once('./' . $api . '/api.php'); 
+			chdir('./' . $api);
+			$GLOBALS['apis'][$api]['init'] = require_once('api.php'); 
 		}
+		chdir($dir);
 	}
 
 	require_once('functions.php');
